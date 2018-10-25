f91a9b4fb190dc49882998c3bc0612f543726945
Sarah Lamonica <sarahlamonica444@gmail.com>
2018-10-25 18:44:39 -0400

    Update Peashooter.java
    Peashooter class with x and y coordinates

--- Peashooter.java
+++ Peashooter.java
@@ -1,5 +1,31 @@
 
 public class Peashooter {
 	private int cost;
+	private int damage;
+	private int positionX;
+	private int positionY;
+	
+	public Peashooter(int cost, int positionX, int positionY)
+	{
+		this.cost = cost;
+		this.positionX = positionX;
+		this.positionY = positionY;
+	}
 
+	public int getPositionX(){
+		return positionX;
+	}
+	
+	public void setPositionX(int positionX){
+		this.positionX = positionX;
+	}
+	
+	public int getPositionY(){
+		return positionY;
+	}
+	
+	public void setPositionY(int positionY){
+		this.positionY = positionY;
+	}
+	
 }
