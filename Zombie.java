2dba63055b00eb60502b834657a6b46e13170818
mounicapillarisetty <43559740+mounicapillarisetty@users.noreply.github.com>
2018-10-25 23:57:59 -0400

    Update Zombie.java

--- Zombie.java
+++ Zombie.java
@@ -1,24 +1,12 @@
-
 public class Zombie {
-	private int lifeRemaining; //Remaining life from zombie
 	private int positionX; // zombie position
 	private int positionY;  // zombie position
 	
 	
-	public Zombie(int lifeRemaining, int positionX, int positionY) {
+	public Zombie(int positionX, int positionY) {
 
-		//super();
-		this.lifeRemaining = lifeRemaining;
 		this.positionX = positionX;
 		this.positionY = positionY;
-	}
-	
-	public int getLifeRemaining(){
-		return lifeRemaining;
-	}
-	
-	public void setLifeRemaining(int lifeRemaining){
-		this.lifeRemaining = lifeRemaining;
 	}
 	
 	public int getPositionX(){
