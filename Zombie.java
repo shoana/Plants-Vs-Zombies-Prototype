77565e86145445fbd2871d5069f08a177889b83b
mounicapillarisetty <43559740+mounicapillarisetty@users.noreply.github.com>
2018-10-25 21:19:14 -0400

    Update Zombie.java

--- Zombie.java
+++ Zombie.java
@@ -35,33 +35,6 @@ public class Zombie {
 	
 	public void setPositionY(int positionY){
 		this.positionY = positionY;
-	}
-	
-	public int eat (){
-		
-	}
-	
-	public int walk (int positionX, int positionY) {
-		if (this.lifeRemaining != 0) { //check if zombie is still living
-			if (positionRow <= 0) { 
-				return -1; // game lost
-			}
-			if () // check is next spot is empty
-				// put zombie in new spot
-				// remove zombie from old spot
-				// update zombie row position (positionRow--)
-				if (){ // check if positionRow <=0
-					return -1; // game lost
-				}
-			}
-			else { // next spot isn't empty
-				if (){ // check what type of plant is there 
-					// clear spot if zombie eats it // may have to have another method 'eat' here
-				}
-			}
-		return 0;
-		}
 	}	
-	
 	
 }
