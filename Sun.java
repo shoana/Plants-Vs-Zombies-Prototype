177efde695aeb9e48ccb3dadacb7818ef86d7eca
shoanasharma <35308640+shoana@users.noreply.github.com>
2018-10-25 20:38:00 -0400

    Update Sun.java

--- Sun.java
+++ Sun.java
@@ -1,34 +1,68 @@
 import java.util.*;
 
 
-class Sun //(?){
-	
-    static Timer timer; //= new Timer();
-    static int seconds; //= 0;
-    private String type = " ";    //Set the object type to indicate which part of the program created the sun
+class Sun{
 
-	 //number is suns
+	private static final int money = 50; //at the start of the game
+	private static final int sun;
+	private int positionX; 
+	private int positionY;
+   	//static Timer timer; //= new Timer();
+    	//static int seconds; //= 0;
+	//number is suns
+		
 	public Sun() {
-		
-		this.type = " ";
-		this.seconds = 0;
-		
+		//this.type = " ";
+		//this.seconds = 0
+	
 	}
 	
-	public void getSun() {
+	public int generateSun(int money){
+		 
+		for(int i; i < 50 ; i++){
+			money += 25;
+			sun = money/25;
+			System.out.println("Now you have: " + sun + " suns!");
+		}
+	
+	}
+	
+	public int getPositionX(){
+		return positionX;
+	}
+	
+	public void setPositionX(int positionX){
+		this.positionX = positionX;
+	}
+	
+	public int getPositionY(){
+		return positionY;
+	}
+	
+	public void setPositionY(int positionY){
+		this.positionY = positionY;
+	}
+	
+	public int getSun() {
+		 return sun;
+	}
 		
-		Random r = new Random();
-		int sun = r.nextInt(10);
+	public int outputSun(){
+	
+		Random outputSun = new Random();
+		int (//the cordinate) = outputSun.nextInt(g);
+	
+	}	//Random r = new Random();
+		//int sun = r.nextInt(10);
 		
 			
-	if (type == "sunflower") //isCollect == true, money from sunflower gets add
-    {
+	if (isCollect() = true){ //sunflower generating the sun and the sun is being randomly generated itself
         //Create a new timer
-        Timer timer = new Timer();
+        //Timer timer = new Timer();
         //Set as single shot
         
     }
-    else
+    else //sun is being randomly generated
     {
         //Start timer to drop the sun
         Timer timer= new Timer();
