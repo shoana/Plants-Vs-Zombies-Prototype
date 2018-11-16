8b9b0f317399ee399225e02325fc54b819092c43
Sarah Lamonica <sarahlamonica444@gmail.com>
2018-11-16 18:04:27 -0500

    Update Controller.java

--- PlantsVsZombies-EventModel/src/Controller.java
+++ PlantsVsZombies-EventModel/src/Controller.java
@@ -2,12 +2,23 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+/**
+ * Controller class
+ * @author sarahlamonica
+ *
+ */
 public class Controller implements ActionListener {
 	
 	gamePlay model;
 	int x;
 	int y;
-
+	
+	/**
+	 * Takes all model variables
+	 * @param model
+	 * @param x
+	 * @param y
+	 */
 	public Controller(gamePlay model, int x, int y)
 	{
 		this.model = model;
@@ -15,6 +26,9 @@ public class Controller implements ActionListener {
 		this.y = y;
 	}
 	
+	/**
+	 * Action performed method will delegate to the model
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		model.plantTurn(x, y);
