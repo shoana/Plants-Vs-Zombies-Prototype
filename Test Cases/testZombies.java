e7581b37c1df4dcad2f1071e6e6bf9fa7561be04
fatimahashi <43560401+fatimahashi@users.noreply.github.com>
2018-11-16 21:04:53 -0500

    Update testZombies.java

--- Test Cases/testZombies.java
+++ Test Cases/testZombies.java
@@ -1,94 +1,88 @@
-import static org.junit.Assert.*;
+import org.junit.Test;
 import java.util.*;
 import org.junit.*;
 import junit.framework.TestCase;
-
 /**
- * This class a part of the test cases which is used to test the functionality of a Zombies and it's edge cases
+ * This class a part of the test cases which is used to test the functionality of 
+ * how a game is over and it's edge cases. It checks if the plants win or zombies win.
  * @author Fatima Hashi, Sarah Lamonica, Shoana Sharma,  Mounica Pillarisetty
- * @version November 16th, 2018
+ * @version November 15th, 2018
  *
  */
-
-//To test health in milestone 3 
-public class testZombies extends TestCase{
-
-	private Zombie z1, z2, z3;
+public class testGameOver extends TestCase {
+	
 	private ArrayList<Zombie> zombies;
-	private boolean zombieAttack;
-	private gamePlay g1;
-	private int numZombies;
-	private int zombiesEaten;
+	private ArrayList<Peashooter> peashooters;
+	private ArrayList<Sunflower> sunflowers;
+	private Zombie z1, z2, z3;
+	private Peashooter p1;
+	private boolean zombieAttack = true; // start of game
 	private int grid [][];
-	private int col = 3, row = 3;
-	
-	
-	@Before
+
 	/**
-	 * This method set's up the array list and the zombies
+	 * This method sets up all the variables used for the test cases
 	 */
 	public void setUp() throws Exception {
 		zombies = new ArrayList<Zombie>();
-		g1 = new gamePlay(5,5, 1000);
-		z1 = new Zombie(10, 10);
-		z2 = new Zombie(20, 20);
-		z3 = new Zombie(30, 30);
-		zombiesEaten = 0;
-		numZombies = 5;
+		peashooters = new ArrayList<Peashooter>();
+		sunflowers = new ArrayList<Sunflower>();
+		p1 = new Peashooter (0, 3, 5); // cost = 0, (x, y) = (3,5)
+		z1 = new Zombie (0,0);
+		z2 = new Zombie (3,4);
+		z3 = new Zombie (3,3);
+		//gamePlay.plantOrZombie();
 	}
 	
 	/**
-	 * This method tests when the game is starting and there are no zombies
+	 * This test case checks if the plants win
 	 */
-	public void testEmptyListSize() {
+	public void testPlantWins() {
 		
-		ArrayList<Zombie> emptyList = zombies;
-		assertEquals("Size of zombie list should be 0", 0, emptyList.size());//edge case when the game board is empty
+		// Zombies are all killed by peashooters while the game has commenced
+		
+		assertTrue("Game over: Plant has won.", zombies.size() == 0 && zombieAttack == true);
+		
 	}
 	
 	/**
-	 * This method tests adding of zombies to the array list 
+	 * This test case checks if the zombies win, when there are no peashooters/sunflowers left on the grid
 	 */
-	public void testAddZombie() {
-		ArrayList<Zombie> addZombie = zombies;
-		addZombie.add(z1);
-		addZombie.add(z2);
-		addZombie.add(z3);
-		assertEquals("Size of zombie list should be 3", 3, addZombie.size());
-	}
-
-	/**
-	 * This method tests the removing of zombies to the array list 
-	 */
-	public void testRemoveZombie() {
-		ArrayList<Zombie> removeZombie = zombies;
-		removeZombie.add(z1);
-		removeZombie.add(z2);
-		removeZombie.add(z3);
-		removeZombie.remove(z1);
-		removeZombie.remove(z2);
-		removeZombie.remove(z3);
-		assertEquals("Size of zombie list should be 0", 0, removeZombie.size());
+	public void testZombieWins1() {
+		
+		zombies.add(z1);
+		// Zombies have eaten all the plants
+		assertTrue("Game over: Zombie has won.", peashooters.size() == 0 && sunflowers.size()==0 && zombies.size() > 0);
+		
 	}
 	
 	/**
-	 * This test case tests it the zombies are attacking 
+	 * This test case checks if the zombies have reached the end and 
+	 * the spaces between the zombie and the plant is no more than 2
 	 */
-	public void testZombieAttack() {
+public void testZombieWins2() {
+		
+		zombies.add(z1);
+		zombies.add(z2);
+		zombies.add(z3);
+		peashooters.add(p1);
+		
+		//Zombies have reached the house
+		assertTrue("Game over: Zombie has won.", z1.getPositionY() == 0);
 		
 
-		ArrayList<Zombie> attackZombie = zombies;
-		gamePlay.zombieTime(5);
-		for(int i = 0; i < numZombies; i++) {
-			zombieAttack = true;
-		}
-		attackZombie.add(z1);
-		assertTrue("Zombies are placed on the grid at the start of the game", zombies.size() < numZombies && zombiesEaten == 0);
-		
+		//Zombie in a row, peashooter not in that row and cannot buy more
+		assertTrue("Game over: Zombie has won.", (z2.getPositionX() == p1.getPositionX() && 
+				(z3.getPositionY() == (p1.getPositionY()) || 
+				(z3.getPositionY() == p1.getPositionY()- 1) || 
+				(z3.getPositionY() == p1.getPositionY()- 2) )));
+	
 	}
+
+
 	@Test
+
 	public static void main(String[] args) {
-		junit.textui.TestRunner.run(testZombies.class);
+		junit.textui.TestRunner.run(testGameOver.class);
 	}
 
 }
