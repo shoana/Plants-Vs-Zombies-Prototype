4c4cb0df4576f406c34d47276be933574f1b30a2
Sarah Lamonica <sarahlamonica444@gmail.com>
2018-11-20 20:25:36 -0500

    Update Sunflower.java

--- Milestone3/Milestone3/Sunflower.java
+++ Milestone3/Milestone3/Sunflower.java
@@ -2,25 +2,69 @@
  * Sunflower class is a type of plant that produces sunshine(money) when you buy the plant.  
  * It allows the user to buy more plants
  * @author Sarah Lamonica, Mounica Pillarisetty, Fatima Hashi, Shoana Sharma 
- * @version November 16th, 2018
+ * @version October 27th, 2018
  */
 
-public class Sunflower extends Plant{
-
-	public static final int cost = 50;
-	public static final String plantType = "sunflower";
-	public static int positionX;
-	public static int positionY;
+public class Sunflower {
+	private int cost; 
+	private int positionX; 
+	private int positionY;
+	private boolean isEaten;
 	
-
 	/**
 	 * Constructor for the Sunflower
 	 * @param cost is an int for the money that is required to buy a peashooter
 	 * @param positionX is an int for x coordinate position on the grid
 	 * @param positionY is an int for y coordinate position on the grip 
 	 */
-	public Sunflower() {
-		super(cost, positionX, positionY, plantType);
+	public Sunflower(int cost, int positionX, int positionY, boolean isEaten) {
+		this.cost = cost;
+		this.positionX = positionX;
+		this.positionY = positionY;
+		this.isEaten = isEaten;
 	}
 
+	/**
+	 * Getter method for position x
+	 * @return int for the position for x
+	 */
+	public int getPositionX(){
+		return positionX;
+	}
+	
+	/**
+	 * Setter method for position x
+	 * @param positionX is an that allows to set the x coordinate on the grid
+	 */
+	public void setPositionX(int positionX){
+		this.positionX = positionX;
+	}
+	
+	/**
+	 * Getter method for position y
+	 * @return int for the position for y
+	 */
+	public int getPositionY(){
+		return positionY;
+	}
+	
+	/**
+	 * Setter method for position y 
+	 * @param positionX is an that allows to set the y coordinate on the grid
+	 */
+	public void setPositionY(int positionY){
+		this.positionY = positionY;
+	}
+	
+	public boolean getEaten()
+	{
+		return isEaten;
+	}
+	
+	public void setEaten()
+	{
+		isEaten = true;
+	}
+	
+	
 }
