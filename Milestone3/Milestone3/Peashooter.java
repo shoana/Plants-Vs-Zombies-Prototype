8c8289fb89adda67d059429404885a1f84c9432f
Sarah Lamonica <sarahlamonica444@gmail.com>
2018-11-20 20:25:17 -0500

    Update Peashooter.java

--- Milestone3/Milestone3/Peashooter.java
+++ Milestone3/Milestone3/Peashooter.java
@@ -2,16 +2,14 @@
  * Peashooter class is a type of plant that attacks the Zombie and prevents them 
  * from entering the house
  * @author Sarah Lamonica, Mounica Pillarisetty, Fatima Hashi, Shoana Sharma 
- * @version November 16th, 2018
+ * @version October 27th, 2018
  */
 
-public class Peashooter extends Plant{
-	
-	public static final int cost = 100;
-	public static int positionX = 0;
-	public static int positionY = 0;
-	public static fianl String plantType = "peashooter";
-	
+public class Peashooter {
+	private int cost;
+	private int positionX;
+	private int positionY;
+	private boolean isEaten;
 	
 	/**
 	 * Constructor for the peashooter
@@ -19,7 +17,50 @@ public class Peashooter extends Plant{
 	 * @param positionX is an int for x coordinate position on the grid
 	 * @param positionY is an int for y coordinate position on the grip 
 	 */
-	public Peashooter() {
-		super(cost, positionX, positionY, plantType);
+	public Peashooter(int cost, int positionX, int positionY, boolean isEaten)
+	{
+		this.cost = cost;
+		this.positionX = positionX;
+		this.positionY = positionY;
+		this.isEaten = false;
+	}
+
+	/**
+	 * Getter method for position x
+	 * @return int for the position for x
+	 */
+	public int getPositionX(){
+		return positionX;
+	}
+	public boolean getEaten()
+	{
+		return isEaten;
+	}
+	public void setEaten()
+	{
+		this.isEaten = true;
+	}
+	/**
+	 * Setter method for position x
+	 * @param positionX is an that allows to set the x coordinate on the grid
+	 */
+	public void setPositionX(int positionX){
+		this.positionX = positionX;
+	}
+	
+	/**
+	 * Getter method for position y
+	 * @return int for the position for y
+	 */
+	public int getPositionY(){
+		return positionY;
+	}
+	
+	/**
+	 * Setter method for position y 
+	 * @param positionX is an that allows to set the y coordinate on the grid
+	 */
+	public void setPositionY(int positionY){
+		this.positionY = positionY;
 	}
 }
