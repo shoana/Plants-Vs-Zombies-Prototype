375c834177af9c2b2b63db7cea929a0e00c9eebb
Sarah Lamonica <sarahlamonica444@gmail.com>
2018-11-20 20:26:53 -0500

    Update Zombie.java

--- Milestone3/Milestone3/Zombie.java
+++ Milestone3/Milestone3/Zombie.java
@@ -2,11 +2,12 @@
  * Zombie attacks the plants preventing themselves from dying and allowing 
  * them to enter the house.
  * @author Sarah Lamonica, Mounica Pillarisetty, Fatima Hashi, Shoana Sharma 
- * @version November 16th, 2018
+ * @version October 27th, 2018
  */
 
 public class Zombie {
 	private int positionX; // zombie position
+	private boolean isEaten;
 	private int positionY;  // zombie position
 	
 	/**
@@ -14,9 +15,10 @@ public class Zombie {
 	 * @param positionX is an int for x coordinate position on the grid
 	 * @param positionY is an int for y coordinate position on the grip 
 	 */
-	public Zombie(int positionX, int positionY) {
+	public Zombie(int positionX, int positionY, boolean isEaten) {
 		this.positionX = positionX;
 		this.positionY = positionY;
+		this.isEaten = isEaten;
 	}
 	
 	/**
@@ -25,6 +27,16 @@ public class Zombie {
 	 */
 	public int getPositionX(){
 		return positionX;
+	}
+	
+	public boolean getEaten()
+	{
+		return isEaten;
+	}
+	
+	public void setEaten()
+	{
+		isEaten = true;
 	}
 	
 	/**
@@ -50,4 +62,10 @@ public class Zombie {
 	public void setPositionY(int positionY){
 		this.positionY = positionY;
 	}	
+	
+	public void move()
+	{
+		this.positionY = positionY - 1;
+		
+	}
 }
