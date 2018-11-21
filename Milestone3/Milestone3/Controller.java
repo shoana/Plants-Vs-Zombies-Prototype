f69bca8ba87f364a7c850e4563f1effaa64dfbea
Sarah Lamonica <sarahlamonica444@gmail.com>
2018-11-20 20:23:16 -0500

    Update Controller.java
    controller code

--- Milestone3/Milestone3/Controller.java
+++ Milestone3/Milestone3/Controller.java
@@ -2,23 +2,24 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 /**
- *Controller class for the MVC model
- *@author Sarah Lamonica, Mounica Pillarisetty, Fatima Hashi, Shoana Sharma 
- *@version November 16th, 2018 
+ * Controller class
+ * @author sarahlamonica
  *
  */
 public class Controller implements ActionListener {
 	
 	gamePlay model;
-	int x;
-	int y;
+	private int x;
+	private int y;
+
 	
 	/**
 	 * Takes all model variables
-	 * @param model a type gamePlay 
-	 * @param x is an type of integers coordiantes
-	 * @param y is an type of integers corordinates
+	 * @param model
+	 * @param x
+	 * @param y
 	 */
 	public Controller(gamePlay model, int x, int y)
 	{
@@ -32,8 +33,15 @@ public class Controller implements ActionListener {
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		model.plantTurn(x, y);
-		model.zombieTime(2);
+		//model.moveZombies();
+		model.plantTurn(x, y); //take a plant turn
+		model.zombieTime(2); //set zombie time (from model)
+		model.plantOrZombie();
+		JButton o = (JButton) e.getSource();
+		
+		
 	}
 	
+	
+
 }
