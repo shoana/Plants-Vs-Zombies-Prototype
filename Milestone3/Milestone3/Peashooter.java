861565c8771f5541d2aadf77b00c37e8db01d4d4
Sarah Lamonica <sarahlamonica444@gmail.com>
2018-11-22 16:32:04 -0500

    Update Peashooter.java
    added subclass

--- Milestone3/Milestone3/Peashooter.java
+++ Milestone3/Milestone3/Peashooter.java
@@ -1,83 +1,14 @@
 /**
- * Zombie attacks the plants preventing themselves from dying and allowing 
- * them to enter the house.
+ * Peashooter
  * @author sarahlamonica
- * @version MILESTONE 3
+ *
  */
 
-public class Zombie {
-	private int positionX; // zombie position
-	private boolean isEaten; //isEaten true or false
-	private int positionY;  // zombie position
-	private int damagePoints;
-	
-	/**
-	 * Constructor for Zombie class
-	 * @param positionX is an int for x coordinate position on the grid
-	 * @param positionY is an int for y coordinate position on the grip 
-	 */
-	public Zombie(int positionX, int positionY, boolean isEaten, int damagePoints) {
-		this.positionX = positionX;
-		this.positionY = positionY;
-		this.isEaten = isEaten;
-		this.damagePoints = damagePoints;
+public class Peashooter extends Plant {
+
+	public Peashooter(int cost, int positionX, int positionY, boolean isEaten) {
+		super(cost, positionX, positionY, isEaten);
+		// TODO Auto-generated constructor stub
 	}
-	
-	/**
-	 * Getter method for position x
-	 * @return int for the position for x
-	 */
-	public int getPositionX(){
-		return positionX;
-	}
-	
-	public boolean getEaten()
-	{
-		return isEaten;
-	}
-	
-	public void setEaten()
-	{
-		isEaten = true;
-	}
-	
-	/**
-	 * Setter method for position x
-	 * @param positionX is an that allows to set the x coordinate on the grid
-	 */
-	public void setPositionX(int positionX){
-		this.positionX = positionX;
-	}
-	
-	/**
-	 * Getter method for position y
-	 * @return int for the position for y
-	 */
-	public int getPositionY(){
-		return positionY;
-	}
-	
-	/**
-	 * Setter method for position y 
-	 * @param positionX is an that allows to set the y coordinate on the grid
-	 */
-	public void setPositionY(int positionY){
-		this.positionY = positionY;
-	}	
-	
-	public int getDmg()
-	{
-		return damagePoints;
-	}
-	
-	public void setDmg(int dmg)
-	{
-		damagePoints = dmg;
-	}
-	
-	
-	public void move()
-	{
-		positionY = positionY - 1;
-	}
+
 }
