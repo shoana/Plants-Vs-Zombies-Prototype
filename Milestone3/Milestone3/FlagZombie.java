9c539cfc058a418a1e8029c622c2368b5d58d3db
Sarah Lamonica <sarahlamonica444@gmail.com>
2018-11-22 16:35:38 -0500

    Add files via upload
    added other plants and zombies

---
+++ Milestone3/Milestone3/FlagZombie.java
@@ -0,0 +1,17 @@
+/**
+ * Flag zombie class
+ * @author sarahlamonica
+ *
+ */
+public class FlagZombie extends Zombie{
+	
+
+	public FlagZombie(int positionX, int positionY, boolean isEaten, int damagePoints) {
+		super(positionX, positionY, isEaten, damagePoints);
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
---
+++ Milestone3/Milestone3/PylonZombie.java
@@ -0,0 +1,13 @@
+/**
+ * Pylon zombie
+ * @author sarahlamonica
+ *
+ */
+public class PylonZombie extends Zombie{
+
+	public PylonZombie(int positionX, int positionY, boolean isEaten, int damagePoints) {
+		super(positionX, positionY, isEaten, damagePoints);
+		// TODO Auto-generated constructor stub
+	}
+
+}
