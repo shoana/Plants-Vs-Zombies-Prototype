c70ad4c64ddad24832348b9261cd2de267b33337
Sarah Lamonica <sarahlamonica444@gmail.com>
2018-11-25 14:47:38 -0500

    Update NormalZombie.java
    More functions

--- Milestone3/Milestone3/NormalZombie.java
+++ Milestone3/Milestone3/NormalZombie.java
@@ -1,14 +1,28 @@
-
+/**
+ * Normal zombie class
+ * @author sarahlamonica
+ *
+ */
 public class NormalZombie extends Zombie {
 
-	public NormalZombie(int positionX, int positionY, boolean isEaten, int damagePoints) {
-		super(positionX, positionY, isEaten, damagePoints);
+	public NormalZombie(int positionX, int positionY, boolean isEaten, int damagePoints, char zombieType, boolean isWalnut) {
+		super(positionX, positionY, isEaten, damagePoints, zombieType, isWalnut);
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * Move function
+	 */
 	public void move()
 	{
 		super.setPositionY(super.getPositionY() - 1);
 	}
 
+	/**
+	 * Gets the type of zombie
+	 */
+	public char getType()
+	{
+		return super.getType();
+	}
 }
