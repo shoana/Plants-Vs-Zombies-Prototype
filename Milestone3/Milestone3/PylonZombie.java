3fe49df4afc5727f6f22d4ed4e5508db02e99acb
Sarah Lamonica <sarahlamonica444@gmail.com>
2018-11-25 14:49:02 -0500

    Update PylonZombie.java

--- Milestone3/Milestone3/PylonZombie.java
+++ Milestone3/Milestone3/PylonZombie.java
@@ -5,14 +5,25 @@
  */
 public class PylonZombie extends Zombie{
 
-	public PylonZombie(int positionX, int positionY, boolean isEaten, int damagePoints) {
-		super(positionX, positionY, isEaten, damagePoints);
+	public PylonZombie(int positionX, int positionY, boolean isEaten, int damagePoints, char zombieType, boolean isWalnut) {
+		super(positionX, positionY, isEaten, damagePoints, zombieType, isWalnut);
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * Move function
+	 */
 	public void move()
 	{
 		super.setPositionY(super.getPositionY() - 1);
+	}
+	
+	/**
+	 * Gets the type of zombie
+	 */
+	public char getType()
+	{
+		return super.getType();
 	}
 
 }
