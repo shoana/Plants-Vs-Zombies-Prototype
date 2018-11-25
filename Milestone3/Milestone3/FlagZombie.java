f2b2ac7a3721e90b8c5bf27b0d18f840a278163f
Sarah Lamonica <sarahlamonica444@gmail.com>
2018-11-25 14:46:20 -0500

    Update FlagZombie.java
    Added some more functions

--- Milestone3/Milestone3/FlagZombie.java
+++ Milestone3/Milestone3/FlagZombie.java
@@ -6,15 +6,25 @@
 public class FlagZombie extends Zombie{
 	
 
-	public FlagZombie(int positionX, int positionY, boolean isEaten, int damagePoints) {
-		super(positionX, positionY, isEaten, damagePoints);
+	public FlagZombie(int positionX, int positionY, boolean isEaten, int damagePoints, char zombieType, boolean isWalnut) {
+		super(positionX, positionY, isEaten, damagePoints, zombieType, isWalnut);
 		
 		// TODO Auto-generated constructor stub
 	}
-
+	/**
+	* Move function
+	*/
 	public void move()
 	{
 		super.setPositionY(super.getPositionY() - 1);
+	}
+	
+	/**
+	* Gets the type of zombie
+	*/
+	public char getType()
+	{
+		return super.getType();
 	}
 
 }
