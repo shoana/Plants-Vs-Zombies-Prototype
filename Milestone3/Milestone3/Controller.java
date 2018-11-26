a6730e65bc20c055af4406f89653e8276ea920c2
shoanasharma <35308640+shoana@users.noreply.github.com>
2018-11-25 19:23:11 -0500

    Update Controller.java

--- Milestone3/Milestone3/Controller.java
+++ Milestone3/Milestone3/Controller.java
@@ -3,32 +3,33 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
-/**
- * Controller class
- * @author sarahlamonica
- *
+/** 
+ * This class sets up Controller of MVC pattern
+ * @author Fatima Hashi, Sarah Lamonica, Shoana Sharma,  Mounica Pillarisetty
+ * @version November 25th, 2018
  */
+
 public class Controller implements ActionListener {
-	
+
 	gamePlay model;
 	private int x;
 	private int y;
-	
-	
+
+
 	/**
 	 * Takes all model variables
-	 * @param model
-	 * @param x
-	 * @param y
+	 * @param model is type of game play  
+	 * @param x an integer for x co-ordinate
+	 * @param y an integer for y co-ordinate
 	 */
 	public Controller(gamePlay model, int x, int y)
 	{
 		this.model = model;
 		this.x = x;
 		this.y = y;
-		
+
 	}
-	
+
 	/**
 	 * Action performed method will delegate to the model
 	 */
@@ -38,7 +39,7 @@ public class Controller implements ActionListener {
 		model.plantTurn(x, y); //take a plant turn
 		model.zombieTime(2); //set zombie time (from model)
 		model.plantsOrZombies(); //check for wins
-		
+
 	}
-	
+
 }
