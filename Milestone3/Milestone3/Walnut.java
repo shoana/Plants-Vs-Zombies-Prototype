3756d220d8a5e7a2891c4d65d6be47f669d9473c
shoanasharma <35308640+shoana@users.noreply.github.com>
2018-11-25 19:26:04 -0500

    Update Walnut.java

--- Milestone3/Milestone3/Walnut.java
+++ Milestone3/Milestone3/Walnut.java
@@ -1,27 +1,36 @@
 
 /**
- * Walnut class
- * @author sarahlamonica
- *
+ * Walnut class is a type of plant that produces extra life
+ * when you buy the plant.  
+ * @author Sarah Lamonica, Mounica Pillarisetty, Fatima Hashi, Shoana Sharma 
+ * @version November 25th, 2018
  */
 public class Walnut extends Plant{
 	private int walnutLife = 0;
+	
+	/**
+	 * Constructor for walnut plant
+	 * @param cost
+	 * @param positionX
+	 * @param positionY
+	 * @param isEaten
+	 */
 	public Walnut(int cost, int positionX, int positionY, boolean isEaten) {
 		super(cost, positionX, positionY, isEaten);
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	/**
-	 * Setter method for increasing lives
+	 * This is a setter method for increasing lives
 	 */
 	public void setLife()
 	{
 		walnutLife += 1;
 	}
-	
+
 	/**
-	 * Getter for walnut's life
-	 * @return
+	 * This is a getter for walnut's life
+	 * @return an int in the form walnut's life 
 	 */
 	public int getLife()
 	{
