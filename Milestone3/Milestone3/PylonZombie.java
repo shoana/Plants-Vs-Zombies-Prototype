5b828f3c4478acd0606ba0a317d3291a6f42e5cd
shoanasharma <35308640+shoana@users.noreply.github.com>
2018-11-25 19:24:54 -0500

    Update PylonZombie.java

--- Milestone3/Milestone3/PylonZombie.java
+++ Milestone3/Milestone3/PylonZombie.java
@@ -1,25 +1,34 @@
 /**
- * Pylon zombie
- * @author sarahlamonica
- *
+ * Pylon Zombie attacks the plants preventing themselves from dying and allowing 
+ * them to enter the house, takes longer than normal zombie.
+ * @author Sarah Lamonica, Mounica Pillarisetty, Fatima Hashi, Shoana Sharma 
+ * @version November 25th, 2018
  */
 public class PylonZombie extends Zombie{
 
+	/**
+	 * Constructor for pylon zombie
+	 * @param positionX is an int for x coordinate position on the grid
+	 * @param positionY is an int for y coordinate position on the grip 
+	 * @param isEaten true if zombie is eaten, false otherwise
+	 * @param damagePoints checks the life of the zombie in form of an integer
+	 * @param isWalnut is the life has been given or taken from zombie 
+	 */
 	public PylonZombie(int positionX, int positionY, boolean isEaten, int damagePoints, char zombieType, boolean isWalnut) {
 		super(positionX, positionY, isEaten, damagePoints, zombieType, isWalnut);
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	/**
-	 * Move function
+	 * This method allows pylon zombies to move function
 	 */
 	public void move()
 	{
 		super.setPositionY(super.getPositionY() - 1);
 	}
-	
+
 	/**
-	 * Gets the type of zombie
+	 * This method allows to get the type of desired zombie
 	 */
 	public char getType()
 	{
