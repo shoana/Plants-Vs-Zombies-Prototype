43804cd91fe2762f9d1f487de7755903d0d2d61e
shoanasharma <35308640+shoana@users.noreply.github.com>
2018-11-25 19:23:42 -0500

    Update FlagZombie.java

--- Milestone3/Milestone3/FlagZombie.java
+++ Milestone3/Milestone3/FlagZombie.java
@@ -1,27 +1,37 @@
 /**
- * Flag zombie class
- * @author sarahlamonica
- *
+ * Flag zombies attacks the plants preventing themselves from dying and allowing 
+ * them to enter the house.
+ * @author Sarah Lamonica, Mounica Pillarisetty, Fatima Hashi, Shoana Sharma 
+ * @version Novemeber 25th, 2018
  */
 public class FlagZombie extends Zombie{
-	
+
+
+	/**
+	 * Constructor for flag zombie
+	 * @param positionX is an int for x coordinate position on the grid
+	 * @param positionY is an int for y coordinate position on the grip 
+	 * @param isEaten true if zombie is eaten, false otherwise
+	 * @param damagePoints checks the life of the zombie in form of an integer
+	 * @param isWalnut is the life has been given or taken from zombie 
+	 */
 
 	public FlagZombie(int positionX, int positionY, boolean isEaten, int damagePoints, char zombieType, boolean isWalnut) {
 		super(positionX, positionY, isEaten, damagePoints, zombieType, isWalnut);
-		
+
 		// TODO Auto-generated constructor stub
 	}
 	/**
-	* Move function
-	*/
+	 * This method allows flag zombies to move function
+	 */
 	public void move()
 	{
 		super.setPositionY(super.getPositionY() - 1);
 	}
-	
+
 	/**
-	* Gets the type of zombie
-	*/
+	 * This method allows to get the type of zombie
+	 */
 	public char getType()
 	{
 		return super.getType();
