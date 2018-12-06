6ba150e3bf36cec973381ed4a00b43eefab76c5e
shoanasharma <35308640+shoana@users.noreply.github.com>
2018-12-06 11:05:44 -0500

    Update Peashooter.java

--- Milestone 4/src/model/Peashooter.java
+++ Milestone 4/src/model/Peashooter.java
@@ -1,14 +1,21 @@
 package model;
 import java.io.Serializable;
-
 /**
- * Peashooter
- * @author sarahlamonica
- *
+ * Peashooter class is a type of plant that attacks the Zombie and prevents them 
+ * from entering the house
+ * @author Sarah Lamonica, Mounica Pillarisetty, Fatima Hashi, Shoana Sharma 
+ * @version December 5th, 2018
  */
 
 public class Peashooter extends Plant implements Serializable {
 
+	/**
+	 * Constructor for cherry bomb
+	 * @param cost is an int for the money that is required to buy a peashooter bomb
+	 * @param positionX is an int for x coordinate position on the grid
+	 * @param positionY is an int for y coordinate position on the grid
+	 * @param isEaten is a boolean to represent cherry bomb being eaten by a zombie 
+	 */
 	public Peashooter(int cost, int positionX, int positionY, boolean isEaten) {
 		super(cost, positionX, positionY, isEaten);
 		// TODO Auto-generated constructor stub
