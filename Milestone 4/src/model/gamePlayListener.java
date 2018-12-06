22b36bc7ea9c9dbdc5981cac2de2c76695622885
shoanasharma <35308640+shoana@users.noreply.github.com>
2018-12-06 11:21:34 -0500

    Update gamePlayListener.java

--- Milestone 4/src/model/gamePlayListener.java
+++ Milestone 4/src/model/gamePlayListener.java
@@ -1,8 +1,8 @@
 package model;
 /**
- * Interface for gamePlayListener
- * @author sarahlamonica
- *
+ *Listener class for the MVC model calls the game event class
+ *@author Sarah Lamonica, Mounica Pillarisetty, Fatima Hashi, Shoana Sharma 
+ *@version December 5th, 2018 
  */
 public interface gamePlayListener {
 	void handleGameEvent(gamePlayEvent e);
