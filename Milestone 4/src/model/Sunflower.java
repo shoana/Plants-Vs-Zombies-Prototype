91ac11dedc16bba78a71d33f486e33776c6499b0
shoanasharma <35308640+shoana@users.noreply.github.com>
2018-12-06 11:09:19 -0500

    Update Sunflower.java

--- Milestone 4/src/model/Sunflower.java
+++ Milestone 4/src/model/Sunflower.java
@@ -2,12 +2,20 @@ package model;
 import java.io.Serializable;
 
 /**
- * Sunflower class
- * @author sarahlamonica
- *
+ * Sunflower class is a type of plant that produces sunshine(money) when you buy the plant.  
+ * It allows the user to buy more plants
+ * @author Sarah Lamonica, Mounica Pillarisetty, Fatima Hashi, Shoana Sharma 
+ * @version December 5th, 2018
  */
 public class Sunflower extends Plant implements Serializable{
 
+	/**
+	 * Constructor for the sunflower class
+	 * @param cost is an int for the money that is required to buy a peashooter
+	 * @param positionX is an int for x coordinate position on the grid
+	 * @param positionY is an int for y coordinate position on the grid 
+	 * @param isEaten is an boolean that sets true is the sunflower is eaten, false otherwise
+	 */
 	public Sunflower(int cost, int positionX, int positionY, boolean isEaten) {
 		super(cost, positionX, positionY, isEaten);
 		// TODO Auto-generated constructor stub
