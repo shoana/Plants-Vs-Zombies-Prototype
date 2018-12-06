e4d5337f8f29f5e652db62152762ee77f9274653
shoanasharma <35308640+shoana@users.noreply.github.com>
2018-12-06 11:10:19 -0500

    Update Walnut.java

--- Milestone 4/src/model/Walnut.java
+++ Milestone 4/src/model/Walnut.java
@@ -2,19 +2,29 @@ package model;
 import java.io.Serializable;
 
 /**
- * Walnut class
- * @author sarahlamonica
- *
+ * Walnut class is a type of plant that produces extra life
+ * when you buy the plant.  
+ * @author Sarah Lamonica, Mounica Pillarisetty, Fatima Hashi, Shoana Sharma 
+ * @version December 5th, 2018
  */
 public class Walnut extends Plant implements Serializable{
+	
 	private int walnutLife = 0;
+	
+	/**
+	 * Constructor for walnut plant
+	 * @param cost
+	 * @param positionX
+	 * @param positionY
+	 * @param isEaten
+	 */
 	public Walnut(int cost, int positionX, int positionY, boolean isEaten) {
 		super(cost, positionX, positionY, isEaten);
 		// TODO Auto-generated constructor stub
 	}
 	
 	/**
-	 * Setter method for increasing lives
+	 * This is a setter method for increasing lives
 	 */
 	public void setLife()
 	{
@@ -22,8 +32,8 @@ public class Walnut extends Plant implements Serializable{
 	}
 	
 	/**
-	 * Getter for walnut's life
-	 * @return
+	 * This is a getter for walnut's life
+	 * @return an int in the form walnut's life 
 	 */
 	public int getLife()
 	{
